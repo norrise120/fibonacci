65b4d5400c285799aa6f6dbb8b187b5383800b80
Erica Norris <norrise120@gmail.com>
2019-04-10 16:06:45 -0700

    Created fibonacci method to pass tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,8 +5,24 @@
 # ....
 # e.g. 6th fibonacci number is 8
 
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n), where n is the integer that is passed in
+# Space complexity: O(1)
 def fibonacci(n)
-  raise NotImplementedError
+  if !n || n < 0
+    raise ArgumentError, "Must pass in an non-negative integer, received: #{n}"
+  end
+
+  return 0 if n == 0
+
+  i = 0
+  j = 1
+
+  while n > 1
+    temp = j + i
+    i = j
+    j = temp
+    n -= 1
+  end
+
+  return j
 end
